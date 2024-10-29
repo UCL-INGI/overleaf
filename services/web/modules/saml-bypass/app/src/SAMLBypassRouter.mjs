234c6ae1d2368583133ccc96ea60cc072dae5c67
Anthony Gégo <anthonygego@hotmail.com>
2024-10-29 16:25:53 +0100

    [web] add saml-bypass module

---
+++ services/web/modules/saml-bypass/app/src/SAMLBypassRouter.mjs
@@ -0,0 +1,24 @@
+import logger from '@overleaf/logger'
+import settings from '@overleaf/settings'
+import SAMLBypassController from './SAMLBypassController.mjs'
+import AuthenticationController from '../../../../app/src/Features/Authentication/AuthenticationController.js'
+
+export default {
+  apply(webRouter) {
+    if(!settings.samlBypass.enabled) {
+      return
+    }
+
+    logger.debug({}, 'Init SAMLBypass router')
+
+    webRouter.get('/sso-login', SAMLBypassController.ssoLoginPage)
+    webRouter.get('/sso-login/metadata', SAMLBypassController.ssoLoginMetadata)
+    webRouter.post('/sso-login/callback', SAMLBypassController.ssoCallback)
+
+    if (AuthenticationController.addEndpointToLoginWhitelist != null) {
+      AuthenticationController.addEndpointToLoginWhitelist('/sso-login')
+      AuthenticationController.addEndpointToLoginWhitelist('/sso-login/metadata')
+      AuthenticationController.addEndpointToLoginWhitelist('/sso-login/callback')
+    }
+  },
+}
